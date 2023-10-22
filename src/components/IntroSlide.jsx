b62dfbfd6bf94e3337c50ee527f184a494155d65
Dogukan Basaran <dogukanbasarann@hotmail.com>
2023-10-22 14:54:22 +0300

    it is updated.

--- src/components/IntroSlide.jsx
+++ src/components/IntroSlide.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
+import introPhoto from '../assets/carousel/1.png'
+import introPhoto2 from '../assets/carousel/2.png'
 
 const Intro = () => {
   return (
 
     <section id='introduction'>
-      <div className='container mx-auto h-[auto] py-10'>
-        <div id='intro-text'>
-              <h1 className='font-heading font-bold text-[48px] text-neutral-800 text-center mb-10'>Eğitim Platformu</h1>
-              <p className='font-text text-[16]px] text-neutral-800 text-center mx-12 mb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In delectus, porro enim voluptatum commodi magnam debitis. Reprehenderit voluptate rerum magni. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi molestias ab commodi sunt temporibus illum, voluptas vitae ullam cumque fugit laboriosam et nihil. Expedita amet reprehenderit dolorum minus veniam? Veritatis earum mollitia nam unde architecto, sint molestiae! Quis labore officiis non, molestiae eum obcaecati minus molestias sunt quod id omnis.</p>  
-        </div>
-        <div id='intro-btn' className='flex justify-center gap-2'>
-              <button className='bg-gray-800 rounded-md text-neutral-300 font-text px-8 sm:px-12 py-2 hover:bg-gray-300 hover:text-neutral-800 duration-300'>Paketlere Göz At</button>
-              <button className='bg-gray-800 rounded-md text-neutral-300 font-text  px-8 sm:px-12 py-2 hover:bg-gray-300 hover:text-neutral-800 duration-300'>İletişime Geç</button>
-        </div>
-      </div>
+      <div className='container grid grid-cols-3 mx-auto h-[auto] py-10'>
 
+      <div id='intro-left' className='flex justify-start'>
+          <img src={introPhoto2} className='w-[400px] h-[400px] ml-10'/>
+
+        </div>
+
+        <div id='intro-mid'>
+              <div id='intro-text'>
+                    <h1 className='font-heading font-bold text-[48px] text-neutral-800 text-center mb-10'>Eğitim Platformu</h1>
+                    <p className='font-text text-[16]px] text-neutral-800 text-center mx-12 mb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In delectus, porro enim voluptatum commodi magnam debitis. Reprehenderit voluptate rerum magni. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi molestias ab commodi sunt temporibus illum, voluptas vitae ullam cumque fugit laboriosam et nihil. Expedita amet reprehenderit dolorum minus veniam? Veritatis earum mollitia nam unde architecto, sint molestiae! Quis labore officiis non, molestiae eum obcaecati minus molestias sunt quod id omnis.</p>  
+              </div>
+              <div id='intro-btn' className='flex justify-center gap-2'>
+                    <button className='bg-gray-800 rounded-md text-neutral-300 font-text px-8 sm:px-12 py-2 hover:bg-gray-300 hover:text-neutral-800 duration-300'>Paketlere Göz At</button>
+                    <button className='bg-gray-800 rounded-md text-neutral-300 font-text  px-8 sm:px-12 py-2 hover:bg-gray-300 hover:text-neutral-800 duration-300'>İletişime Geç</button>
+              </div>
+        </div>
+
+        <div id='intro-right' className='flex justify-end'>
+          <img src={introPhoto} className='w-[400px] h-[400px]'/>
+
+        </div>
+
+
+      </div>
     </section>
   )
 }
--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
+import whyChooseUsPhoto from '../assets/carousel/3.png'
+import whyChooseUsPhoto2 from '../assets/carousel/4.png'
 
 const WhyChooseUs = () => {
   return (
     <section id='why-u-choose-us'>
-         <div className='container mx-auto h-[auto] py-10'>
-                <div id='intro-text'>
-                    <h1 className='font-heading font-bold text-[48px] text-neutral-800 text-center mb-10'>Neden Bizi Seçmelisin?</h1>
-                    <p className='font-text text-[16]px] text-neutral-800 text-center mx-12 mb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In delectus, porro enim voluptatum commodi magnam debitis. Reprehenderit voluptate rerum magni. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi molestias ab commodi sunt temporibus illum, voluptas vitae ullam cumque fugit laboriosam et nihil. Expedita amet reprehenderit dolorum minus veniam? Veritatis earum mollitia nam unde architecto, sint molestiae! Quis labore officiis non, molestiae eum obcaecati minus molestias sunt quod id omnis.</p>  
-                </div>
-                <div id='intro-btn' className='flex justify-center gap-2'>
-                    <button className='bg-gray-800 rounded-md text-neutral-300 font-text font-semib px-12 py-2 hover:bg-gray-300 hover:text-neutral-800 duration-300'>Daha Fazlasını Oku</button>
-                </div>
+         <div className='container grid grid-cols-3 mx-auto h-[auto] py-10 items-center'>
+
+              <div id='choose-us-left' className='hidden xl:block'>
+                  <img src={whyChooseUsPhoto2} className='w-[400px] h-[auto]'/>
+              </div>
+
+               <div id='choose-us-mid'>
+                  <div id='intro-text'>
+                        <h1 className='font-heading font-bold text-[48px] text-neutral-800 text-center mb-10'>Neden Bizi Seçmelisin?</h1>
+                        <p className='font-text text-[16]px] text-neutral-800 text-center mx-12 mb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In delectus, porro enim voluptatum commodi magnam debitis. Reprehenderit voluptate rerum magni. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi molestias ab commodi sunt temporibus illum, voluptas vitae ullam cumque fugit laboriosam et nihil. Expedita amet reprehenderit dolorum minus veniam? Veritatis earum mollitia nam unde architecto, sint molestiae! Quis labore officiis non, molestiae eum obcaecati minus molestias sunt quod id omnis.</p>  
+                    </div>
+                    <div id='intro-btn' className='flex justify-center gap-2'>
+                        <button className='bg-gray-800 rounded-md text-neutral-300 font-text font-semib px-12 py-2 hover:bg-gray-300 hover:text-neutral-800 duration-300'>Daha Fazlasını Oku</button>
+                    </div>
+               </div>
+
+               <div id='choose-us-right' className='hidden xl:block'>
+                  <img src={whyChooseUsPhoto} className='w-[400px] h-[350px]'/>
+              </div>
         </div>
     </section>
   )
