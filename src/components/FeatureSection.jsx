fff634c01db69fa2ac1df27edd5dd51d6d2ab7ce
Dogukan Basaran <dogukanbasarann@hotmail.com>
2023-10-28 15:22:18 +0300

    it is updated.

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -4,44 +4,49 @@ import {MdExpandMore} from 'react-icons/md'
 
 const FeatureSection = () => {
   return (
-    <section className='h-[auto]  my-24 mb-52'>
-        <div className='container grid grid-cols-4 gap-4'>
+    <section className='my-24'>
 
-            <div className='rounded-md relative'>
-                <img src={photo1} className='rounded-xl'/>
-                <div className='rounded-2xl border absolute bg-white w-3/4 left-[45px] top-[180px] shadow-xl'>
+        <h2 className='text-center text-[32px] md:text-[48px] font-semibold font-heading text-neutral-800'>Neden Biz?</h2>
+
+        <div className='container py-10 h-[auto] mx-auto flex flex-col justify-center items-center md:grid md:grid-cols-2 md:grid-rows-2 xl:grid-cols-4 px-10 gap-4 border border-black'>
+
+            <div className='rounded-md relative max-w-[350px]'>
+                <img src={photo1} className='rounded-xl mb-20'/>
+                <div className='z-50 rounded-2xl border absolute bg-white w-3/4 left-[48px] top-[170px] lg:top-[180px] xl:top-[140px] 2xl:top-[180px] shadow-xl'>
                     <h3 className='text-center text-neutral-800 text-[24px] font-heading font-bold cursor-pointer'>Birebir Özel Ders</h3>
-                    <p className='text-sm text-center px-2 font-text'>Bizde sınıf ve tahta yok. Yalnızca öğrenci ve profesyonel öğretmen var. Tüm derslerimiz bire bir özel ders formatıyla yapılır. Bizde saat yok. Ders, öğrenci konuyu anlayınca biter. Ücretsiz özel ders randevusu için tıklayınız.</p>
+                    <p className='hidden text-sm text-center px-2 font-text'>Bizde sınıf ve tahta yok. Yalnızca öğrenci ve profesyonel öğretmen var. Tüm derslerimiz bire bir özel ders formatıyla yapılır. Bizde saat yok. Ders, öğrenci konuyu anlayınca biter. Ücretsiz özel ders randevusu için tıklayınız.</p>
                     <span><MdExpandMore className='text-[40px] w-full cursor-pointer'/></span>
                 </div>
             </div>
 
-            <div className='rounded-md relative'>
-                <img src={photo1} className='rounded-xl'/>
-                <div className='rounded-2xl border absolute bg-white w-3/4 left-[45px] top-[180px] shadow-xl'>
+            <div className='rounded-md relative max-w-[350px]'>
+                <img src={photo1} className='rounded-xl mb-20'/>
+                <div className='z-50 rounded-2xl border absolute bg-white w-3/4 left-[48px] top-[170px] lg:top-[180px] xl:top-[140px] 2xl:top-[180px] shadow-xl'>
                     <h3 className='text-center text-neutral-800 text-[24px] font-heading font-bold cursor-pointer'>Birebir Özel Ders</h3>
-                    <p className='text-sm text-center px-2 font-text'>Bizde sınıf ve tahta yok. Yalnızca öğrenci ve profesyonel öğretmen var. Tüm derslerimiz bire bir özel ders formatıyla yapılır. Bizde saat yok. Ders, öğrenci konuyu anlayınca biter. Ücretsiz özel ders randevusu için tıklayınız.</p>
+                    <p className='hidden text-sm text-center px-2 font-text'>Bizde sınıf ve tahta yok. Yalnızca öğrenci ve profesyonel öğretmen var. Tüm derslerimiz bire bir özel ders formatıyla yapılır. Bizde saat yok. Ders, öğrenci konuyu anlayınca biter. Ücretsiz özel ders randevusu için tıklayınız.</p>
                     <span><MdExpandMore className='text-[40px] w-full cursor-pointer'/></span>
                 </div>
             </div>
 
-            <div className='rounded-md relative'>
-                <img src={photo1} className='rounded-xl'/>
-                <div className='rounded-2xl border absolute bg-white w-3/4 left-[45px] top-[180px] shadow-xl'>
+            <div className='rounded-md relative max-w-[350px]'>
+                <img src={photo1} className='rounded-xl mb-20'/>
+                <div className='z-50 rounded-2xl border absolute bg-white w-3/4 left-[48px] top-[170px] lg:top-[180px] xl:top-[140px] 2xl:top-[180px] shadow-xl'>
                     <h3 className='text-center text-neutral-800 text-[24px] font-heading font-bold cursor-pointer'>Birebir Özel Ders</h3>
-                    <p className='text-sm text-center px-2 font-text'>Bizde sınıf ve tahta yok. Yalnızca öğrenci ve profesyonel öğretmen var. Tüm derslerimiz bire bir özel ders formatıyla yapılır. Bizde saat yok. Ders, öğrenci konuyu anlayınca biter. Ücretsiz özel ders randevusu için tıklayınız.</p>
+                    <p className='hidden text-sm text-center px-2 font-text'>Bizde sınıf ve tahta yok. Yalnızca öğrenci ve profesyonel öğretmen var. Tüm derslerimiz bire bir özel ders formatıyla yapılır. Bizde saat yok. Ders, öğrenci konuyu anlayınca biter. Ücretsiz özel ders randevusu için tıklayınız.</p>
                     <span><MdExpandMore className='text-[40px] w-full cursor-pointer'/></span>
                 </div>
             </div>
 
-            <div className='rounded-md relative'>
-                <img src={photo1} className='rounded-xl'/>
-                <div className='rounded-2xl border absolute bg-white w-3/4 left-[45px] top-[180px] shadow-xl'>
+            <div className='rounded-md relative max-w-[350px]'>
+                <img src={photo1} className='rounded-xl mb-20'/>
+                <div className='z-50 rounded-2xl border absolute bg-white w-3/4 left-[48px] top-[170px] lg:top-[180px] xl:top-[140px] 2xl:top-[180px] shadow-xl'>
                     <h3 className='text-center text-neutral-800 text-[24px] font-heading font-bold cursor-pointer'>Birebir Özel Ders</h3>
-                    <p className='text-sm text-center px-2 font-text'>Bizde sınıf ve tahta yok. Yalnızca öğrenci ve profesyonel öğretmen var. Tüm derslerimiz bire bir özel ders formatıyla yapılır. Bizde saat yok. Ders, öğrenci konuyu anlayınca biter. Ücretsiz özel ders randevusu için tıklayınız.</p>
+                    <p className='hidden text-sm text-center px-2 font-text'>Bizde sınıf ve tahta yok. Yalnızca öğrenci ve profesyonel öğretmen var. Tüm derslerimiz bire bir özel ders formatıyla yapılır. Bizde saat yok. Ders, öğrenci konuyu anlayınca biter. Ücretsiz özel ders randevusu için tıklayınız.</p>
                     <span><MdExpandMore className='text-[40px] w-full cursor-pointer'/></span>
                 </div>
             </div>
+
+           
 
         </div>
     </section>
