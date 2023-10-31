eadb134f2c6b441ddc317dff3b919ba735fc1649
Dogukan Basaran <dogukanbasarann@hotmail.com>
2023-10-31 03:41:02 +0300

    feature updated.

--- src/data/features.jsx
+++ src/data/features.jsx
@@ -1,4 +1,7 @@
 import photo1 from '../assets/features/feature1.jpg'
+import photo2 from '../assets/features/feature2.jpg'
+import photo3 from '../assets/features/feature3.jpg'
+import photo4 from '../assets/features/feature4.jpg'
 
 
 const features = [
@@ -13,7 +16,7 @@ const features = [
     id: 2,
     title: "BİREBİR ÖZEL DERS",
     detailedText: "Bizde sınıf ve tahta yok. Yalnızca öğrenci ve profesyonel öğretmen var. Tüm derslerimiz bire bir özel ders formatıyla yapılır. Bizde saat yok. Ders, öğrenci konuyu anlayınca biter. Ücretsiz özel ders randevusu için tıklayınız.",
-    featureImage: photo1
+    featureImage: photo2
     
   },
   {
@@ -27,7 +30,7 @@ const features = [
     id: 4,
     title: "BİREBİR ÖZEL DERS",
     detailedText: "Bizde sınıf ve tahta yok. Yalnızca öğrenci ve profesyonel öğretmen var. Tüm derslerimiz bire bir özel ders formatıyla yapılır. Bizde saat yok. Ders, öğrenci konuyu anlayınca biter. Ücretsiz özel ders randevusu için tıklayınız.",
-    featureImage: photo1
+    featureImage: photo2
     
   }
 ]
