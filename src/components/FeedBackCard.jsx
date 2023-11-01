f2cdd5e72c5228ece2fa3ead800b553a2a8b64c9
Dogukan Basaran <dogukanbasarann@hotmail.com>
2023-11-01 18:05:24 +0300

    it is improved.

--- src/components/FeedBackCard.jsx
+++ src/components/FeedBackCard.jsx
@@ -1,69 +1,29 @@
 import React from 'react'
 import {AiFillStar} from 'react-icons/ai'
+import feedbacksData from '../data/feedback'
 
 const FeedBackCard = () => {
   return (
 
-    <div className='flex flex-col justify-center items-center lg:flex-row gap-4'>
+    <div className='grid  px-10 py-5 sm:px-0 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-rows-1 gap-5 lg:flex-row'>
 
-            <div className='h-[150px] w-[300px] rounded-md shadow-lg bg-gray-100 border-[0.5px] hover:scale-105 duration-300 cursor-pointer'>
-                <h4 className='font-semibold font-text text-[18px] pl-2 mb-2'>Sümeyye T.</h4>
-                <p className='text-center font-text'>Mükemmel ilgili hocalar, üst düzey bilgi seviyesi. Herkese tavsiye ederim.</p>
-                <div className='flex justify-center mt-2'>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
+        {feedbacksData.map((feedback) => {
+            return (
+                <div className='px-2 sm:mb-8 h-[250px] lg:w-[300px] xl:w-[300px] overflow-hidden rounded-md shadow-lg bg-gray-100 border-[0.5px] hover:scale-105 duration-300 cursor-pointer' key={feedback.id}>
+                        <h4 className='font-semibold font-text text-[18px] pl-2 mb-2'>{feedback.name}</h4>
+                        <p className='text-center font-text'>{feedback.feedback}</p>
+                        <div className='flex justify-center mt-2'>
+                            <AiFillStar className='text-[20px] text-yellow-500'/>
+                            <AiFillStar className='text-[20px] text-yellow-500'/>
+                            <AiFillStar className='text-[20px] text-yellow-500'/>
+                            <AiFillStar className='text-[20px] text-yellow-500'/>
+                            <AiFillStar className='text-[20px] text-yellow-500'/>
+                        </div>
                 </div>
-            </div>
+            )
+        })}
+          
 
-            <div className='h-[150px] w-[300px] rounded-md shadow-lg bg-gray-100 border-[0.5px] hover:scale-105 duration-300 cursor-pointer'>
-                <h4 className='font-semibold font-text text-[18px] pl-2 mb-2'>Sümeyye T.</h4>
-                <p className='text-center font-text'>Mükemmel ilgili hocalar, üst düzey bilgi seviyesi. Herkese tavsiye ederim.</p>
-                <div className='flex justify-center mt-2'>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                </div>
-            </div>
-
-            <div className='h-[150px] w-[300px] rounded-md shadow-lg bg-gray-100 border-[0.5px] hover:scale-105 duration-300 cursor-pointer'>
-                <h4 className='font-semibold font-text text-[18px] pl-2 mb-2'>Sümeyye T.</h4>
-                <p className='text-center font-text'>Mükemmel ilgili hocalar, üst düzey bilgi seviyesi. Herkese tavsiye ederim.</p>
-                <div className='flex justify-center mt-2'>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                </div>
-            </div>
-            <div className='h-[150px] w-[300px] rounded-md shadow-lg bg-gray-100 border-[0.5px] hover:scale-105 duration-300 cursor-pointer'>
-                <h4 className='font-semibold font-text text-[18px] pl-2 mb-2'>Sümeyye T.</h4>
-                <p className='text-center font-text'>Mükemmel ilgili hocalar, üst düzey bilgi seviyesi. Herkese tavsiye ederim.</p>
-                <div className='flex justify-center mt-2'>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                </div>
-            </div>
-
-            <div className='h-[150px] w-[300px] rounded-md shadow-lg bg-gray-100 border-[0.5px] hover:scale-105 duration-300 cursor-pointer'>
-                <h4 className='font-semibold font-text text-[18px] pl-2 mb-2'>Sümeyye T.</h4>
-                <p className='text-center font-text'>Mükemmel ilgili hocalar, üst düzey bilgi seviyesi. Herkese tavsiye ederim.</p>
-                <div className='flex justify-center mt-2'>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                    <AiFillStar className='text-[20px] text-yellow-500'/>
-                </div>
-            </div>
 
     </div>
   )
---
+++ src/data/feedback.jsx
@@ -0,0 +1,53 @@
+
+const feedbacks = [
+    {
+        id: 1,
+        name: "Sümeyye T.",
+        feedback: "Mükemmel ilgili hocalar, üst düzey bilgi seviyesi. Herkese tavsiye ederim."
+    },
+
+    {
+        id: 2,
+        name: "Fatma S.",
+        feedback: "Gittim ve kaydoldum. Hocalarım çok iyi! Çok güzel bir eğitim alacağımdan eminim. Herkese tavsiye ederim."
+    },
+
+    {
+        id: 3,
+        name: "Celil Ş.",
+        feedback: "Bir tanıdığım aracılığı ile öğrendim. İlk görüşmedeki ilgi alaka, ders çalışma planı ve programları çok ilgimi çekti. Hem ailem hem ben, gönül rahatlığı ile tercihimizi buradan yana yaptık."
+    },
+
+    {
+        id: 4,
+        name: "Hatice Y.",
+        feedback: "Matematiğe gönül vermiş idealist gençlerle tanışmaktan çok memnun oldum. Normalde bir saat ancak konsantre olup ders yapabilen oğlum 9. sınıf konu anlatımı sonu çözüm takıldığı yerde müdahale ile eksiklerini tamamlayacak inşallah."
+    },
+
+    {
+        id: 5,
+        name: "Utkan Ç.",
+        feedback: "Hocam buralara gelmem hep sizin sayenizde, bana matematiği sevdiren hocam."
+    },
+
+    {
+        id: 6,
+        name: "Handen Ç.",
+        feedback: 'Özgür Aydın Akademi gerçekten dershanelere göre çok daha kaliteli bir yer. Sloganları da çok güzel. "Siz üniversiteyi seçin gerisi bizde."'
+    },
+
+    {
+        id: 7,
+        name: "Şevval Rümeysa B.",
+        feedback: "Okuldan aldığım eğitimle notlarım çok düşükken Özgür Hocam sayesinde notlarım çok kısa sürede yükseldi. Hem eğitimi hem samimiyeti için çok teşekkürler."
+    },
+
+    {
+        id: 8,
+        name: "Güleda S.",
+        feedback: "Zamanında Özgür Hocadan ders aldırmıştım. Çocuğum Tıp-Cerrahpaşa kazandı. Gerçekten çok iyi bir matematikçi."
+    },
+
+]
+
+export default feedbacks
